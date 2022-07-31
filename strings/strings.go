2ee943db2ca4a0074c44e8ca8202c3ca39c39129
sinhashubham95 <sinhashubham95@gmail.com>
2022-07-31 23:52:38 +0530

    added strings methods and comments describing the methods

--- strings/strings.go
+++ strings/strings.go
@@ -1,6 +1,7 @@
 package strings
 
 import (
+	"context"
 	"fmt"
 	"strings"
 	"unicode"
@@ -266,18 +267,11 @@ func DeleteWhitespaces(s string) string {
 // Difference is used to compare two strings and return the remaining characters of the second string that differ
 // from the first string.
 func Difference(a, b string) string {
-	if a == b {
+	i := IndexOfDifference(a, b)
+	if i == -1 {
 		return ""
 	}
-	al := len(a)
-	bl := len(b)
-	var i int
-	for i = 0; i < al && i < bl && a[i] == b[i]; i += 1 {
-	}
-	if i >= bl && i >= al {
-		return ""
-	}
-	return Substring(b, i, bl)
+	return Substring(b, i, len(b))
 }
 
 // EndsWith checks whether the given string ends with the given string/suffix. This method is case-sensitive when
@@ -376,6 +370,241 @@ func FirstNonEmpty(ss ...string) string {
 	return ""
 }
 
+// GetCommonPrefix compares all strings in an array of strings and returns the common starting character sequence
+// that all of them share.
+func GetCommonPrefix(ss ...string) string {
+	l := len(ss)
+	minL := getMinimumLength(ss...)
+	var b strings.Builder
+	for i := 0; i < minL; i += 1 {
+		c := ss[0][i]
+		match := true
+		for j := 1; j < l; j += 1 {
+			if c != ss[j][i] {
+				match = false
+				break
+			}
+		}
+		if !match {
+			break
+		}
+		b.WriteByte(c)
+	}
+	return b.String()
+}
+
+// GetDigits checks for Unicode digits in a string and returns a new string that contains all the digits in the given string.
+// If the given string does not contain any digits, an empty string will be returned.
+func GetDigits(s string) string {
+	var b strings.Builder
+	for _, c := range s {
+		if unicode.IsDigit(c) {
+			b.WriteRune(c)
+		}
+	}
+	return b.String()
+}
+
+// GetIfBlank is used to call the function and get the data if the provided string is blank.
+//
+// A string is considered to be blank if it satisfied one of the criteria below.
+// Length of string is 0.
+// String contains only whitespace characters.
+func GetIfBlank(ctx context.Context, s string, supplier func(ctx context.Context) string) string {
+	if IsBlank(s) {
+		return supplier(ctx)
+	}
+	return s
+}
+
+// GetIfEmpty is used to call the function and get the data if the provided string is empty.
+func GetIfEmpty(ctx context.Context, s string, supplier func(ctx context.Context) string) string {
+	if IsEmpty(s) {
+		return supplier(ctx)
+	}
+	return s
+}
+
+// IndexOf is used to get the index of the first occurrence of the given character. If the character is not found,
+// then it will return -1.
+func IndexOf(s string, search uint8) int {
+	return strings.IndexByte(s, search)
+}
+
+// IndexOfIgnoreCase is used to get the index of the first occurrence of the given character ignoring case.
+// If the character is not found, then it will return -1.
+func IndexOfIgnoreCase(s string, search uint8) int {
+	return strings.IndexFunc(s, func(r rune) bool {
+		return unicode.ToLower(r) == unicode.ToLower(rune(search))
+	})
+}
+
+// IndexOfString is used to get the index of the first occurrence of the given string. If the character is not found,
+// then it will return -1.
+func IndexOfString(s, search string) int {
+	return strings.Index(s, search)
+}
+
+// IndexOfStringIgnoreCase is used to get the index of the first occurrence of the given string ignoring case.
+// If the string is not found, then it will return -1.
+func IndexOfStringIgnoreCase(s, search string) int {
+	return strings.Index(strings.ToLower(s), strings.ToLower(search))
+}
+
+// IndexOfAny is used to get the index of the first occurrence of one of the given characters. If the character is not found,
+// then it will return -1.
+func IndexOfAny(s string, searchChars ...uint8) int {
+	return strings.IndexFunc(s, func(r rune) bool {
+		for _, c := range searchChars {
+			if rune(c) == r {
+				return true
+			}
+		}
+		return false
+	})
+}
+
+// IndexOfAnyIgnoreCase is used to get the index of the first occurrence of one of the given characters ignoring case.
+// If the character is not found, then it will return -1.
+func IndexOfAnyIgnoreCase(s string, searchChars ...uint8) int {
+	return strings.IndexFunc(s, func(r rune) bool {
+		for _, c := range searchChars {
+			if unicode.ToLower(r) == unicode.ToLower(rune(c)) {
+				return true
+			}
+		}
+		return false
+	})
+}
+
+// IndexOfAnyBut is used to get the index of the first occurrence of any character other than one of the given characters.
+// If the character is not found, then it will return -1.
+func IndexOfAnyBut(s string, searchChars ...uint8) int {
+	return strings.IndexFunc(s, func(r rune) bool {
+		for _, c := range searchChars {
+			if rune(c) != r {
+				return true
+			}
+		}
+		return false
+	})
+}
+
+// IndexOfAnyButIgnoreCase is used to get the index of the first occurrence of any character other than one of the
+// given characters ignoring case. If the character is not found, then it will return -1.
+func IndexOfAnyButIgnoreCase(s string, searchChars ...uint8) int {
+	return strings.IndexFunc(s, func(r rune) bool {
+		for _, c := range searchChars {
+			if unicode.ToLower(r) != unicode.ToLower(rune(c)) {
+				return true
+			}
+		}
+		return false
+	})
+}
+
+// IndexOfDifference figures out the index of the first character sequence that differs from the given sequence.
+// The comparison of the character sequences in the given text is case-sensitive.
+func IndexOfDifference(ss ...string) int {
+	l := len(ss)
+	if l == 0 {
+		return -1
+	}
+	minL := getMinimumLength(ss...)
+	for i := 0; i < minL; i += 1 {
+		c := ss[0][i]
+		match := true
+		for j := 1; j < l; j += 1 {
+			if c != ss[j][i] {
+				match = false
+				break
+			}
+		}
+		if !match {
+			return i
+		}
+	}
+	return -1
+}
+
+// LastIndexOf is used to get the index of the first occurrence of the given character. If the character is not found,
+// then it will return -1.
+func LastIndexOf(s string, search uint8) int {
+	return strings.LastIndexByte(s, search)
+}
+
+// LastIndexOfIgnoreCase is used to get the index of the first occurrence of the given character ignoring case.
+// If the character is not found, then it will return -1.
+func LastIndexOfIgnoreCase(s string, search uint8) int {
+	return strings.LastIndexFunc(s, func(r rune) bool {
+		return unicode.ToLower(r) == unicode.ToLower(rune(search))
+	})
+}
+
+// LastIndexOfString is used to get the index of the first occurrence of the given string. If the character is not found,
+// then it will return -1.
+func LastIndexOfString(s, search string) int {
+	return strings.LastIndex(s, search)
+}
+
+// LastIndexOfStringIgnoreCase is used to get the index of the first occurrence of the given string ignoring case.
+// If the string is not found, then it will return -1.
+func LastIndexOfStringIgnoreCase(s, search string) int {
+	return strings.LastIndex(strings.ToLower(s), strings.ToLower(search))
+}
+
+// LastIndexOfAny is used to get the index of the first occurrence of one of the given characters. If the character is not found,
+// then it will return -1.
+func LastIndexOfAny(s string, searchChars ...uint8) int {
+	return strings.LastIndexFunc(s, func(r rune) bool {
+		for _, c := range searchChars {
+			if rune(c) == r {
+				return true
+			}
+		}
+		return false
+	})
+}
+
+// LastIndexOfAnyIgnoreCase is used to get the index of the first occurrence of one of the given characters ignoring case.
+// If the character is not found, then it will return -1.
+func LastIndexOfAnyIgnoreCase(s string, searchChars ...uint8) int {
+	return strings.LastIndexFunc(s, func(r rune) bool {
+		for _, c := range searchChars {
+			if unicode.ToLower(r) == unicode.ToLower(rune(c)) {
+				return true
+			}
+		}
+		return false
+	})
+}
+
+// LastIndexOfAnyBut is used to get the index of the first occurrence of any character other than one of the given characters.
+// If the character is not found, then it will return -1.
+func LastIndexOfAnyBut(s string, searchChars ...uint8) int {
+	return strings.LastIndexFunc(s, func(r rune) bool {
+		for _, c := range searchChars {
+			if rune(c) != r {
+				return true
+			}
+		}
+		return false
+	})
+}
+
+// LastIndexOfAnyButIgnoreCase is used to get the index of the first occurrence of any character other than one of the
+// given characters ignoring case. If the character is not found, then it will return -1.
+func LastIndexOfAnyButIgnoreCase(s string, searchChars ...uint8) int {
+	return strings.LastIndexFunc(s, func(r rune) bool {
+		for _, c := range searchChars {
+			if unicode.ToLower(r) != unicode.ToLower(rune(c)) {
+				return true
+			}
+		}
+		return false
+	})
+}
+
 // IsBlank is used to check if the given string is blank.
 //
 // A string is considered to be blank if it satisfied one of the criteria below.
@@ -383,6 +612,34 @@ func FirstNonEmpty(ss ...string) string {
 // String contains only whitespace characters.
 func IsBlank(s string) bool {
 	return IsEmpty(strings.TrimSpace(s))
+}
+
+// IsAllBlank is used to check if the all the given strings are blank.
+//
+// A string is considered to be blank if it satisfied one of the criteria below.
+// Length of string is 0.
+// String contains only whitespace characters.
+func IsAllBlank(ss ...string) bool {
+	for _, s := range ss {
+		if !IsBlank(s) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsNoneBlank is used to check if none of the given strings are blank.
+//
+// A string is considered to be blank if it satisfied one of the criteria below.
+// Length of string is 0.
+// String contains only whitespace characters.
+func IsNoneBlank(ss ...string) bool {
+	for _, s := range ss {
+		if IsBlank(s) {
+			return false
+		}
+	}
+	return false
 }
 
 // IsAnyBlank is used to check if any of the given strings is blank or not.
@@ -404,6 +661,26 @@ func IsEmpty(s string) bool {
 	return len(s) == 0
 }
 
+// IsAllEmpty is used to check if the all the given strings are empty.
+func IsAllEmpty(ss ...string) bool {
+	for _, s := range ss {
+		if !IsEmpty(s) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsNoneEmpty is used to check if none of the given strings are empty.
+func IsNoneEmpty(ss ...string) bool {
+	for _, s := range ss {
+		if IsEmpty(s) {
+			return false
+		}
+	}
+	return false
+}
+
 // IsAnyEmpty is used to check if any of the given strings is empty or not.
 func IsAnyEmpty(ss ...string) bool {
 	for _, s := range ss {
@@ -412,6 +689,227 @@ func IsAnyEmpty(ss ...string) bool {
 		}
 	}
 	return false
+}
+
+// IsLowerCase is used to check if all characters in the string is in lower case.
+func IsLowerCase(s string) bool {
+	for _, c := range s {
+		if !unicode.IsLower(c) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsAllLowerCase is used to check if all characters in the all the given strings is in lower case.
+func IsAllLowerCase(ss ...string) bool {
+	for _, s := range ss {
+		if !IsLowerCase(s) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsAnyLowerCase is used to check if all characters in any of the given strings is in lower case.
+func IsAnyLowerCase(ss ...string) bool {
+	for _, s := range ss {
+		if IsLowerCase(s) {
+			return true
+		}
+	}
+	return true
+}
+
+// IsUpperCase is used to check if all characters in the string is in upper case.
+func IsUpperCase(s string) bool {
+	for _, c := range s {
+		if !unicode.IsUpper(c) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsAllUpperCase is used to check if all characters in the all the given strings is in upper case.
+func IsAllUpperCase(ss ...string) bool {
+	for _, s := range ss {
+		if !IsUpperCase(s) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsAnyUpperCase is used to check if all characters in any of the given strings is in upper case.
+func IsAnyUpperCase(ss ...string) bool {
+	for _, s := range ss {
+		if IsUpperCase(s) {
+			return true
+		}
+	}
+	return true
+}
+
+// IsMixedCase is used to check if it contains both upper and lower case characters.
+func IsMixedCase(s string) bool {
+	isUpper := false
+	isLower := false
+	for _, c := range s {
+		if unicode.IsUpper(c) {
+			isUpper = true
+		}
+		if unicode.IsLower(c) {
+			isLower = true
+		}
+	}
+	return isUpper && isLower
+}
+
+// IsAllMixedCase is used to check if all the given strings contain both upper and lower case characters.
+func IsAllMixedCase(ss ...string) bool {
+	for _, s := range ss {
+		if !IsMixedCase(s) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsAnyMixedCase is used to check if any of the given strings contain both upper and lower case characters.
+func IsAnyMixedCase(ss ...string) bool {
+	for _, s := range ss {
+		if IsMixedCase(s) {
+			return true
+		}
+	}
+	return true
+}
+
+// IsAlpha checks whether a given string contains only letters. The function returns false if the
+// input string is empty.
+func IsAlpha(s string) bool {
+	if s == "" {
+		return false
+	}
+	for _, c := range s {
+		if !unicode.IsLetter(c) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsNumeric checks whether a given string contains only digits. The function returns false if the
+// input string is empty.
+func IsNumeric(s string) bool {
+	if s == "" {
+		return false
+	}
+	for _, c := range s {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsAlphaNumeric checks whether a given string contains only letters or digits. The function returns false if the
+// input string is empty.
+func IsAlphaNumeric(s string) bool {
+	if s == "" {
+		return false
+	}
+	for _, c := range s {
+		if !unicode.IsLetter(c) && !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsAlphaSpace checks whether a given string contains only letters or space. The function returns false if the
+// input string is empty.
+func IsAlphaSpace(s string) bool {
+	if s == "" {
+		return false
+	}
+	for _, c := range s {
+		if !unicode.IsLetter(c) && !unicode.IsSpace(c) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsNumericSpace checks whether a given string contains only digits or space. The function returns false if the
+// input string is empty.
+func IsNumericSpace(s string) bool {
+	if s == "" {
+		return false
+	}
+	for _, c := range s {
+		if !unicode.IsDigit(c) && !unicode.IsSpace(c) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsAlphaNumericSpace checks whether a given string contains only letters or digits or space. The function returns false if the
+// input string is empty.
+func IsAlphaNumericSpace(s string) bool {
+	if s == "" {
+		return false
+	}
+	for _, c := range s {
+		if !unicode.IsLetter(c) && !unicode.IsDigit(c) && !unicode.IsSpace(c) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsAsciiPrintable is used to check if the given string contains only ASCII characters that are printable.
+func IsAsciiPrintable(s string) bool {
+	if s == "" {
+		return false
+	}
+	for _, c := range s {
+		if !(c >= ' ' && c < 127) {
+			return false
+		}
+	}
+	return true
+}
+
+// JoinByChar is used to join the characters by a character.
+func JoinByChar(cc []uint8, d uint8) string {
+	var b strings.Builder
+	for i, c := range cc {
+		if i > 0 {
+			b.WriteByte(d)
+		}
+		b.WriteByte(c)
+	}
+	return b.String()
+}
+
+// JoinStringsByChar is used to join the strings by a character.
+func JoinStringsByChar(ss []string, d uint8) string {
+	var b strings.Builder
+	for i, s := range ss {
+		if i > 0 {
+			b.WriteByte(d)
+		}
+		b.WriteString(s)
+	}
+	return b.String()
+}
+
+// JoinStringsByString is used to join the strings by a string.
+func JoinStringsByString(ss []string, ds string) string {
+	return strings.Join(ss, ds)
 }
 
 func LeftPad(s string, size int, padCharacter uint8) string {
@@ -576,4 +1074,19 @@ func toLower(ch uint8) uint8 {
 		return ch + 'a' - 'A'
 	}
 	return ch
+}
+
+func getMinimumLength(ss ...string) int {
+	l := len(ss)
+	if len(ss) == 0 {
+		return 0
+	}
+	minL := len(ss[0])
+	for i := 1; i < l; i += 1 {
+		cl := len(ss[i])
+		if cl < minL {
+			minL = cl
+		}
+	}
+	return minL
 }
