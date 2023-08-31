774141b3c0da5d33ca72e1e10e91132ed8638776
sinhashubham95 <sinhashubham95@gmail.com>
2023-08-31 12:43:58 +0530

    added test cases

--- regex/regex_test.go
+++ regex/regex_test.go
@@ -18,3 +18,80 @@ func TestExpand(t *testing.T) {
 	option3: value3
 `), []byte("$key=$value\n"), regexp.MustCompile(`(?m)(?P<key>\w+):\s+(?P<value>\w+)$`), 1))
 }
+
+func TestExpandWithPattern(t *testing.T) {
+	assert.Equal(t, []byte("option1=value1\n"), regex.ExpandWithPattern([]byte(`
+	# comment line
+	option1: value1
+	option2: value2
+
+	# another comment line
+	option3: value3
+`), []byte("$key=$value\n"), `(?m)(?P<key>\w+):\s+(?P<value>\w+)$`, 1))
+}
+
+func TestExpandString(t *testing.T) {
+	assert.Equal(t, "option1=value1\n", regex.ExpandString(`
+	# comment line
+	option1: value1
+	option2: value2
+
+	# another comment line
+	option3: value3
+`, "$key=$value\n", regexp.MustCompile(`(?m)(?P<key>\w+):\s+(?P<value>\w+)$`), 1))
+}
+
+func TestExpandStringWithPattern(t *testing.T) {
+	assert.Equal(t, "option1=value1\n", regex.ExpandStringWithPattern(`
+	# comment line
+	option1: value1
+	option2: value2
+
+	# another comment line
+	option3: value3
+`, "$key=$value\n", `(?m)(?P<key>\w+):\s+(?P<value>\w+)$`, 1))
+}
+
+func TestExpandAll(t *testing.T) {
+	assert.Equal(t, []byte("option1=value1\noption2=value2\noption3=value3\n"), regex.ExpandAll([]byte(`
+	# comment line
+	option1: value1
+	option2: value2
+
+	# another comment line
+	option3: value3
+`), []byte("$key=$value\n"), regexp.MustCompile(`(?m)(?P<key>\w+):\s+(?P<value>\w+)$`)))
+}
+
+func TestExpandAllWithPattern(t *testing.T) {
+	assert.Equal(t, []byte("option1=value1\noption2=value2\noption3=value3\n"), regex.ExpandAllWithPattern([]byte(`
+	# comment line
+	option1: value1
+	option2: value2
+
+	# another comment line
+	option3: value3
+`), []byte("$key=$value\n"), `(?m)(?P<key>\w+):\s+(?P<value>\w+)$`))
+}
+
+func TestExpandAllString(t *testing.T) {
+	assert.Equal(t, "option1=value1\noption2=value2\noption3=value3\n", regex.ExpandAllString(`
+	# comment line
+	option1: value1
+	option2: value2
+
+	# another comment line
+	option3: value3
+`, "$key=$value\n", regexp.MustCompile(`(?m)(?P<key>\w+):\s+(?P<value>\w+)$`)))
+}
+
+func TestExpandAllStringWithPattern(t *testing.T) {
+	assert.Equal(t, "option1=value1\noption2=value2\noption3=value3\n", regex.ExpandAllStringWithPattern(`
+	# comment line
+	option1: value1
+	option2: value2
+
+	# another comment line
+	option3: value3
+`, "$key=$value\n", `(?m)(?P<key>\w+):\s+(?P<value>\w+)$`))
+}
